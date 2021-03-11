ff030505c4c2c1faca71ff2d99ed925a223194a0
@regix2003 <reginaldodeoliveirademello@gmail.com>
2021-03-10 22:58:53 -0300

    updated test list, extract employee to separated file and created test set

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -0,0 +1,20 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for(salario in salarios){
+        println(salario)
+    }
+
+    println("------------------------------------------------------------")
+    println("Maior salarios: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
+    println("Media salarial: ${salarios.average()}")
+
+    val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
+
+    println("------------------------------------------------------------")
+    salariosMaiorQue2500.forEach { println(it) }
+
+}
